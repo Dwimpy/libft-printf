110fd1140f6c3970c4992e9c95be501deebd3dd5
arobu <arobu@student.42heilbronn.de>
2023-04-19 03:04:39 +0200

    update

--- src/ft_stdlib/ft_strlen.c
+++ src/ft_stdlib/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: arobu <arobu@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 19:29:55 by arobu             #+#    #+#             */
-/*   Updated: 2022/10/17 02:09:26 by arobu            ###   ########.fr       */
+/*   Updated: 2023/04/14 18:17:51 by arobu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_strlen(const char *s)
 	unsigned int	count;
 
 	count = 0;
-	while (*s)
+	while (*s != '\0')
 	{
 		count++;
 		s++;
